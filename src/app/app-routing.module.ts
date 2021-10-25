9f9117f26086a904c6371c7bb50e804410c4dc08
karan <karan@git.com>
2021-10-25 22:18:55 +0530

    designed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookmarksComponent } from './components/bookmarks/bookmarks.component';
+import { NotesComponent } from './components/notes/notes.component';
+import { TodoComponent } from './components/todo/todo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'bookmarks', pathMatch: 'full' },
+  {path:'bookmarks', component:BookmarksComponent},
+  {path:'todo', component:TodoComponent},
+  {path:'notes', component:NotesComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingPath=[BookmarksComponent,
+                          TodoComponent,
+                          NotesComponent
+                        ]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingPath } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './components/header/header.component';
+import { TabsComponent } from './components/tabs/tabs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TabsComponent,
+    routingPath,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
